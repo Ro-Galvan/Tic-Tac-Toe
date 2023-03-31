488c8a7f2bbec522165c1c2d947d00e44279902d
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-03-31 15:15:39 -0400

    added switch case for X input

--- Game/src/main/java/com/tictactoe/Game/GameApplication.java
+++ Game/src/main/java/com/tictactoe/Game/GameApplication.java
@@ -29,10 +29,37 @@ public class GameApplication {
 
 		switch (input) {
 			case "1":
-			gameBoard[0][0] = 'X';
+				gameBoard[0][0] = 'X';
 				break;
+			case "2":
+				gameBoard[0][1] = 'X';
+				break;
+			case "3":
+				gameBoard[0][2] = 'X';
+				break;
+			case "4":
+				gameBoard[1][0] = 'X';
+				break;
+			case "5":
+				gameBoard[1][1] = 'X';
+				break;
+			case "6":
+				gameBoard[1][2] = 'X';
+				break;
+			case "7":
+				gameBoard[2][0] = 'X';
+				break;
+			case "8":
+				gameBoard[2][1] = 'X';
+				break;
+			case "9":
+				gameBoard[2][2] = 'X';
+				break;
+			default:
+				System.out.println("Please try a valid input");
 		}
 
+		printBoard(gameBoard);
 	}
 
 	//	adding symbols for the board game

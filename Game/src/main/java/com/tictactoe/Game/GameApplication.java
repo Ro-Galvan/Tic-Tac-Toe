49695f3be44aa5906e9a31ab9c1500f1cb66c950
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-03-31 17:03:47 -0400

    added another switch case statement to check if the position on board is available

--- Game/src/main/java/com/tictactoe/Game/GameApplication.java
+++ Game/src/main/java/com/tictactoe/Game/GameApplication.java
@@ -3,6 +3,7 @@ package com.tictactoe.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
 import java.util.Scanner;
 
 //@SpringBootApplication
@@ -21,9 +22,49 @@ public class GameApplication {
 
 //		adding Scanner class so the user can be prompted to enter X or O
 		Scanner scanner = new Scanner(System.in);
+//		extracted the method for switch case so it can be reused
+
+		playerTurn(gameBoard, scanner);
+
+		Random random = new Random();
+		int computerPlay = random.nextInt(9) + 1;
+
+		printBoard(gameBoard);
+	}
+
+	private static boolean ifAvailable (char[][] gameBoard, int position) {
+		switch (position) {
+			case 1:
+				if (gameBoard[0][0] == ' ') {
+					return true;
+				} else {
+					return false;
+				}
+			case 2:
+//				BETTER WAY TO SHORTEN A TRADITIONAL IF ELSE STATEMENT AND STILL DOES THE SAME THING
+				return (gameBoard[0][1] == ' ');
+			case 3:
+				return (gameBoard[0][2] == ' ');
+			case 4:
+				return (gameBoard[1][0] == ' ');
+			case 5:
+				return (gameBoard[1][1] == ' ');
+			case 6:
+				return (gameBoard[1][2] == ' ');
+			case 7:
+				return (gameBoard[2][0] == ' ');
+			case 8:
+				return (gameBoard[2][1] == ' ');
+			case 9:
+				return (gameBoard[2][2] == ' ');
+			default:
+				return false;
+		}
+	}
+
+	private static void playerTurn(char[][] gameBoard, Scanner scanner) {
 		System.out.println("Please select a number on the board where you would like to place your game piece?");
 		String input = scanner.nextLine();
-		System.out.println(input);
 
 //		Using switch case for options on where to place game piece
 
@@ -58,8 +99,6 @@ public class GameApplication {
 			default:
 				System.out.println("Please try a valid input");
 		}
-
-		printBoard(gameBoard);
 	}
 
 	//	adding symbols for the board game
